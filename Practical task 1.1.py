92cad1692da48f3739f339636c97d7b23e060632
matrosovmn <matrosovmn@gmail.com>
2022-06-27 16:54:16 +0300

    Practical task 1.1 добавлен код

---
+++ Practical task 1.1.py
@@ -0,0 +1,6 @@
+# Тест
+name = input("Как Вас зовут? ")
+birth = int(input("В каком году Вы родились? "))
+year = int(input("Какой сейчас год ? "))
+result = year - birth + 1
+print(name + ", уже в следующем году Вам будет " + str(result) + " лет" + " :-(")
