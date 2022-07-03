25a503ee89f08f3a5822d623a2d09f9896994867
matrosovmn <matrosovmn@gmail.com>
2022-07-04 01:07:31 +0300

    Practical task 3.2 на проверку

---
+++ Practical task 3.2.py
@@ -0,0 +1,18 @@
+name = input("Введите имя :")
+surname = input("Введите фамилию :")
+year = input("Введите год рождения :")
+city = input("Введите город проживания :")
+email = input("Введите email :")
+telephone = input("Введите телефон :")
+
+
+def personal(name, surname, year, city, email, telephone):
+    return ' '.join(["имя: ", name,
+                     "фамилия: ", surname,
+                     "год рождения: ", year,
+                     "город проживания: ", city,
+                     "email: ", email,
+                     "телефон: ", telephone])
+
+
+print(personal(name=name, surname=surname, year=year, city=city, email=email, telephone=telephone))
