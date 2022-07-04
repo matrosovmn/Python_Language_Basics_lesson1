ff5360af81c2b6bc97715f8789bb68ee53f5a3d4
matrosovmn <matrosovmn@gmail.com>
2022-07-04 23:26:42 +0300

    Practical task 3.1 на проверку-добавлена функция округления

--- Practical task 3.1.py
+++ Practical task 3.1.py
@@ -1,13 +1,12 @@
-while True:
-    def division_numbers(*args):
-        try:
-            number1 = float(input("Введите число 1 : "))
-            number2 = float(input("Введите число 2, отличное от нуля : "))
-            result = number1 / number2
-        except ValueError:
-            return "Введены некорректные данные"
-        except ZeroDivisionError:
-            return "Деление на ноль невозможно"
-        return result
-    print(f"Результат деления - {division_numbers()}")
-    # break
+def division_numbers(*args):
+    try:
+        number1 = float(input("Введите число 1 : "))
+        number2 = float(input("Введите число 2, отличное от нуля : "))
+        result = number1 / number2
+    except ValueError:
+        return "Введены некорректные данные"
+    except ZeroDivisionError:
+        return "Деление на ноль невозможно"
+    return round(result, 4)
+print(f"Результат деления - {division_numbers()}")
+
