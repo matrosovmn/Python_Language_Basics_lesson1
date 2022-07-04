53158689316082b8570cfde1a32f21a258385573
matrosovmn <matrosovmn@gmail.com>
2022-07-04 23:49:10 +0300

    Practical task 3.2 на проверку - оптимизировано через словарь

--- Practical task 3.2.py
+++ Practical task 3.2.py
@@ -1,9 +1,9 @@
-name = input("Введите имя :")
-surname = input("Введите фамилию :")
-year = input("Введите год рождения :")
-city = input("Введите город проживания :")
-email = input("Введите email :")
-telephone = input("Введите телефон :")
+# name = input("Введите имя :")
+# surname = input("Введите фамилию :")
+# year = input("Введите год рождения :")
+# city = input("Введите город проживания :")
+# email = input("Введите email :")
+# telephone = input("Введите телефон :")
 
 
 def personal(name, surname, year, city, email, telephone):
@@ -14,5 +14,16 @@ def personal(name, surname, year, city, email, telephone):
                      "email: ", email,
                      "телефон: ", telephone])
 
+# print(personal(name=name, surname=surname, year=year, city=city, email=email, telephone=telephone))
 
-print(personal(name=name, surname=surname, year=year, city=city, email=email, telephone=telephone))
+params = {
+"name": input("Введите имя :"),
+"surname": input("Введите фамилию :"),
+"year": input("Введите год рождения :"),
+"city": input("Введите город проживания :"),
+"email": input("Введите email :"),
+"telephone": input("Введите телефон :"),
+}
+
+# Оператор ** соберёт все переданные именованные аргументы в словарь - очень круто!!!
+print(personal(**params))
