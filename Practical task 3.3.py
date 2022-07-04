928872731bebd7261ff68c0f9a9690e9f1feec28
matrosovmn <matrosovmn@gmail.com>
2022-07-05 00:03:54 +0300

    Practical task 3.3 на проверку - причесан код

--- Practical task 3.3.py
+++ Practical task 3.3.py
@@ -1,22 +1,19 @@
 def my_func(*args):
-    args1 = int(input("Введите целое число 1 : "))
-    args2 = int(input("Введите целое число 2 :"))
-    args3 = int(input("Введите целое число 3 :"))
+    num1 = int(input("Введите целое число 1 : "))
+    num2 = int(input("Введите целое число 2 : "))
+    num3 = int(input("Введите целое число 3 : "))
 
-    if args1 >= args2 > args3:
-        return args1 + args2
-    elif args2 >= args1 > args3:
-        return args2 + args1
-    elif args3 >= args2 > args1:
-        return args3 + args2
-    elif args3 >= args1 > args2:
-        return args3 + args1
-    elif args1 >= args3 > args2:
-        return args1 + args3
-    elif args2 >= args3 > args1:
-        return args2 + args3
-    # else:
-    #     exit(1)
-
+    if num1 >= num2 > num3:
+        return num1 + num2
+    elif num2 >= num1 > num3:
+        return num2 + num1
+    elif num3 >= num2 > num1:
+        return num3 + num2
+    elif num3 >= num1 > num2:
+        return num3 + num1
+    elif num1 >= num3 > num2:
+        return num1 + num3
+    elif num2 >= num3 > num1:
+        return num2 + num3
 
 print(f"Cумма наибольших двух введенных чисел : {my_func()}")
