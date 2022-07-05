e017818053a969e0e3ef65fec59f3f9e8da56dd5
matrosovmn <matrosovmn@gmail.com>
2022-07-06 00:28:26 +0300

    Practical task 3.7 на проверку - вынос мозга с латиницей

--- Practical task 3.7.py
+++ Practical task 3.7.py
@@ -1,5 +1,10 @@
-def int_func(*args):
-    word = input("Введите через пробел несколько слов маленькими буквами: ")
-    print(word.title())
-    return
-int_func()
+def int_func(word):
+    latin_letters = "qwertyuiopasdfghjklzxcvbnm"
+    return word.title() if not set(word).difference(latin_letters) else False
+
+
+words = input("Введите через пробел несколько слов маленькими латинскими буквами: ").split()
+for el in words:
+    result = int_func(el)
+    if result:
+        print(int_func(el), ' ')
