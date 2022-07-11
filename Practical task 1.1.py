f777d6f33c8e8749240faee0f3374bfca7673ac2
matrosovmn <102472485+matrosovmn@users.noreply.github.com>
2022-07-11 11:03:23 +0300

    Delete Practical task 1.1.py

--- Practical task 1.1.py
+++
@@ -1,6 +0,0 @@
-
-name = input("Как Вас зовут? ")
-birth = int(input("В каком году Вы родились? "))
-year = int(input("Какой сейчас год ? "))
-result = year - birth + 1
-print(name + ", уже в следующем году Вам будет " + str(result) + " лет" + " :-(")
